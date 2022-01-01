f7709f22443d6dbf52a78d3d99ac9fb68064b51d
Luis <lmr.87@protonmail.ch>
2022-01-01 21:38:10 +0100

    Método formateaNombre clase Paciente
    Se crea el método formateaNombre, que elimina los caracteres en blanco sobrantes y pone las primeras
    letras de cada palabra en mayúscula

--- src/main/java/org/iesalandalus/programacion/citasclinica/Paciente.java
+++ src/main/java/org/iesalandalus/programacion/citasclinica/Paciente.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.citasclinica;
 
+import java.util.Locale;
+
 public class Paciente {
     private static final String ER_DNI = "/^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKE]$/i";
     private static final String ER_TELEFONO = "(\\+34|0034|34)?[ -]*(6|7|8|9)[ -]*([0-9][ -]*){8}";
@@ -7,7 +9,30 @@ public class Paciente {
     private String dni;
     private String telefono;
 
-    private String formatearNombre(String nombre) {
+    /** Método para formatear los nombres, eliminando caracteres en blanco, y poniendo las primeras letras en mayúscula */
+    private String formateaNombre(String nombre) {
+        // Primero quitamos espacios al principio y al final
+        nombre = nombre.trim();
 
+        // Sustituimos las secuencias de dos o mas espacios por un solo espacio
+        nombre = nombre.replaceAll("\\s{2,}", " ");
+
+        // Divido el nombre en un array, para separar por palabras, separado por espacios en blanco
+        String[] partesNombre = nombre.split(" ");
+
+        // Vacío el nombre para reconstruirlo
+        nombre = "";
+
+        // Bucle para recorrer el array partesNombre
+        for (String partes : partesNombre) {
+            // Por cada parte, pongo en mayúscula la posición 0 y en minúscula las demás, añado un espacio al final.
+            partes = partes.substring(0, 1).toUpperCase() + partes.substring(1).toLowerCase() + " ";
+
+            // Reconstruyo el nombre
+            nombre += partes;
+        }
+
+        // Devuelvo el nombre borrándole el espacio final que se creó en el bucle
+        return nombre.trim();
     }
 }
