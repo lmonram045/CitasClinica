3779e4a0f9d92b1e8c883b0e2f4ca7bf06b75a58
Luis <lmr.87@protonmail.ch>
2022-01-12 23:42:10 +0100

    Arreglo clase Paciente y metodo comprobarLetrarDni
    - Cambio la expresión regular del dni por otra mas apropiada.
    - Creo el método comprobarLetraDni en el que compruebo si el dni está escrito y formateado
     correctamente y si su letra es correcta

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+@
+CitasClinica.iml,7\a\7a658541c7181e2e32c825bfe058a607b288fd29
--- src/main/java/org/iesalandalus/programacion/citasclinica/Paciente.java
+++ src/main/java/org/iesalandalus/programacion/citasclinica/Paciente.java
@@ -1,9 +1,11 @@
 package org.iesalandalus.programacion.citasclinica;
 
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Paciente {
-    private static final String ER_DNI = "/^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKE]$/i";
+    private static final String ER_DNI = "[0-9]{7,8}[A-Z a-z]"; // Se puede mejorar
     private static final String ER_TELEFONO = "(\\+34|0034|34)?[ -]*(6|7|8|9)[ -]*([0-9][ -]*){8}";
     private String nombre;
     private String dni;
@@ -34,5 +36,158 @@ public class Paciente {
 
         // Devuelvo el nombre borrándole el espacio final que se creó en el bucle
         return nombre.trim();
+    }
+
+    /** Método para comprobar la letra del dni */
+    private boolean comprobarLetraDni(String dni) {
+        // Primero comprobamos si es nulo
+        if (dni == null)
+            throw new NullPointerException("ERROR: El DNI de un paciente no puede ser nulo o vacío.");
+
+        // Quitamos los posibles espacios en blanco del dni
+        dni = dni.trim();
+
+        // Creamos las variables pat y mat para comprobar si el dni coincide con la expresión regular
+        Pattern pat = Pattern.compile(ER_DNI);
+        Matcher mat = pat.matcher(dni);
+
+        // Comprobamos que el dni coincide con la expresión regular y tiene el tamaño adecuado
+        if (dni.length() != 9 || !mat.matches())
+            throw new IllegalArgumentException("ERROR: El DNI no tiene un formato válido.");
+
+        // Asigno el dni a un dni auxiliar para sacarle el número
+        String dniAux = dni;
+        dniAux = dniAux.replaceAll("[^0-9]", "");
+
+        // Convierto el numero del dni en un entero
+        int numDni = Integer.parseInt(dniAux);
+
+        // Compruebo la letra
+        String letra = dni.substring(dni.length() - 1); // Primero saco la letra de el string de el dni a una variable
+        // La letra del dni se asigna a partir de el resto de dividir el numero entre 23
+        int resto = numDni % 23;
+        switch (resto) {
+            case 0:
+                if (!letra.equals("T") && !letra.equals("t"))
+                    throw new IllegalArgumentException("ERROR: La letra del DNI no es correcta.");
+                break;
+
+            case 1:
+                if (!letra.equals("R") && !letra.equals("r"))
+                    throw new IllegalArgumentException("ERROR: La letra del DNI no es correcta.");
+                break;
+
+            case 2:
+                if (!letra.equals("W") && !letra.equals("w"))
+                    throw new IllegalArgumentException("ERROR: La letra del DNI no es correcta.");
+                break;
+
+            case 3:
+                if (!letra.equals("A") && !letra.equals("a"))
+                    throw new IllegalArgumentException("ERROR: La letra del DNI no es correcta.");
+                break;
+
+            case 4:
+                if (!letra.equals("G") && !letra.equals("g"))
+                    throw new IllegalArgumentException("ERROR: La letra del DNI no es correcta.");
+                break;
+
+            case 5:
+                if (!letra.equals("M") && !letra.equals("m"))
+                    throw new IllegalArgumentException("ERROR: La letra del DNI no es correcta.");
+                break;
+
+            case 6:
+                if (!letra.equals("Y") && !letra.equals("y"))
+                    throw new IllegalArgumentException("ERROR: La letra del DNI no es correcta.");
+                break;
+
+            case 7:
+                if (!letra.equals("F") && !letra.equals("f"))
+                    throw new IllegalArgumentException("ERROR: La letra del DNI no es correcta.");
+                break;
+
+            case 8:
+                if (!letra.equals("P") && !letra.equals("p"))
+                    throw new IllegalArgumentException("ERROR: La letra del DNI no es correcta.");
+                break;
+
+            case 9:
+                if (!letra.equals("D") && !letra.equals("d"))
+                    throw new IllegalArgumentException("ERROR: La letra del DNI no es correcta.");
+                break;
+
+            case 10:
+                if (!letra.equals("X") && !letra.equals("x"))
+                    throw new IllegalArgumentException("ERROR: La letra del DNI no es correcta.");
+                break;
+
+            case 11:
+                if (!letra.equals("B") && !letra.equals("b"))
+                    throw new IllegalArgumentException("ERROR: La letra del DNI no es correcta.");
+                break;
+
+            case 12:
+                if (!letra.equals("N") && !letra.equals("n"))
+                    throw new IllegalArgumentException("ERROR: La letra del DNI no es correcta.");
+                break;
+
+            case 13:
+                if (!letra.equals("J") && !letra.equals("j"))
+                    throw new IllegalArgumentException("ERROR: La letra del DNI no es correcta.");
+                break;
+
+            case 14:
+                if (!letra.equals("Z") && !letra.equals("z"))
+                    throw new IllegalArgumentException("ERROR: La letra del DNI no es correcta.");
+                break;
+
+            case 15:
+                if (!letra.equals("S") && !letra.equals("s"))
+                    throw new IllegalArgumentException("ERROR: La letra del DNI no es correcta.");
+                break;
+
+            case 16:
+                if (!letra.equals("Q") && !letra.equals("q"))
+                    throw new IllegalArgumentException("ERROR: La letra del DNI no es correcta.");
+                break;
+
+            case 17:
+                if (!letra.equals("V") && !letra.equals("v"))
+                    throw new IllegalArgumentException("ERROR: La letra del DNI no es correcta.");
+                break;
+
+            case 18:
+                if (!letra.equals("H") && !letra.equals("h"))
+                    throw new IllegalArgumentException("ERROR: La letra del DNI no es correcta.");
+                break;
+
+            case 19:
+                if (!letra.equals("L") && !letra.equals("l"))
+                    throw new IllegalArgumentException("ERROR: La letra del DNI no es correcta.");
+                break;
+
+            case 20:
+                if (!letra.equals("C") && !letra.equals("c"))
+                    throw new IllegalArgumentException("ERROR: La letra del DNI no es correcta.");
+                break;
+
+            case 21:
+                if (!letra.equals("K") && !letra.equals("k"))
+                    throw new IllegalArgumentException("ERROR: La letra del DNI no es correcta.");
+                break;
+
+            case 22:
+                if (!letra.equals("E") && !letra.equals("e"))
+                    throw new IllegalArgumentException("ERROR: La letra del DNI no es correcta.");
+                break;
+
+            default:
+                break;
+        }
+
+        // Si no salta ninguna excepción quiere decir que la letra está bien, asi que devuelvo "true"
+        return true;
+
     }
 }
