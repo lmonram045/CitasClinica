c9e439b3bacce07650fda452427eb52a2314189a
Luis <lmr.87@protonmail.ch>
2022-01-13 01:40:46 +0100

    Creados los metodos get y set de la clase Cita

--- src/main/java/org/iesalandalus/programacion/citasclinica/Cita.java
+++ src/main/java/org/iesalandalus/programacion/citasclinica/Cita.java
@@ -1,10 +1,40 @@
 package org.iesalandalus.programacion.citasclinica;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Cita {
-    // por lo pronto voy a fomratear la hora como dd/mm/yyyy hh:mm o dd-mm-yyyy hh:mm
-    private static final String FORMATO_FECHA_HORA = "^([0-2][0-9]|3[0-1])(\\/|-)(0[1-9]|1[0-2])\\2(\\d{4})(\\s)([0-1][0-9]|2[0-3])(:)([0-5][0-9])$";
+    private static final String FORMATO_FECHA_HORA = "dd/mm/yyyy hh:mm";
     private LocalDateTime fechaHora;
+    private Paciente paciente;
+
+    private void setPaciente(Paciente paciente) {
+        if (paciente == null)
+            throw new NullPointerException("ERROR: El paciente de una cita no puede ser nulo.");
+
+        this.paciente = paciente;
+    }
+
+    public Paciente getPaciente() {
+        return new Paciente(paciente);
+    }
+
+    public void setFechaHora(LocalDateTime fechaHora) {
+        if (fechaHora == null)
+            throw new NullPointerException("ERROR: La fecha y hora de una cita no puede ser nula.");
+
+        this.fechaHora = fechaHora;
+    }
+
+    public LocalDateTime getFechaHora() {
+        return fechaHora;
+    }
 
 }
+/*
+    // Variable para poder mostrar la fecha y hora en el formato que queremos
+    DateTimeFormatter f = DateTimeFormatter.ofPattern("dd/mm/yy hh:mm");
+
+        fechaHora = fechaHora.format(f);*/
