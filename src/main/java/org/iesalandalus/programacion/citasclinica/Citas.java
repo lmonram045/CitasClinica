80c580df73cbccafe82cf27f46afe4f3af2fb192
Luis <lmr.87@protonmail.ch>
2022-01-19 00:38:17 +0100

    punto iv clase Citas
    Creado el método buscarIndice de la clase Citas

--- src/main/java/org/iesalandalus/programacion/citasclinica/Citas.java
+++ src/main/java/org/iesalandalus/programacion/citasclinica/Citas.java
@@ -3,13 +3,29 @@ package org.iesalandalus.programacion.citasclinica;
 public class Citas {
     private int capacidad;
     private int tamano;
+    private Cita[] coleccionCitas;
+
+
+    public int getTamano() {
+        return tamano;
+    }
 
     public int getCapacidad() {
         return capacidad;
     }
 
-    public int getTamano() {
-        return tamano;
+    /** Método para buscar un índice */
+    private int buscarIndice(Cita cita) {
+        int indice = 0;
+        // For each para recorrer las citas y sacar buscar un índice
+        for (Cita citas : coleccionCitas) {
+            if (cita.equals(citas))
+                return indice;
+
+            indice++;
+        }
+
+        return indice;
     }
 
     private boolean tamanoSuperado(int indice) {
